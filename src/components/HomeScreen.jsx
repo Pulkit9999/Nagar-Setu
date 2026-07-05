780fca3179020c47ce0d1b3fb7d3b15abb16768e
Pulkit Acharya <pullkit.9@gmail.com>
2026-07-06 01:34:30 +0530

    Banner issue fixed on Home Screen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import MainLayout from "../layouts/MainLayout";
 import "../css/HomeScreen.css";
-
+import banner from "../assets/nagar-setu-banner-ok.webp";
 const HomeScreen = () => {
   return (
     <MainLayout showSessionTimer={false}>
-      
+      <img src={banner} className="banner-image" alt="Nagar Setu Banner" />
     </MainLayout>
   );
 };
