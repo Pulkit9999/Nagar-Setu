7713339f42d16362e3d4d323bf9816abcdbfcb4d
Pulkit Acharya <pullkit.9@gmail.com>
2026-07-17 21:46:30 +0530

    Made some changes in firebase js utils file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,16 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyARvm6ZkH_Z0cnwxKTvpTpkq8mzBEIAxzc",
-//   authDomain: "nagar-setu-a67a7.firebaseapp.com",
-//   projectId: "nagar-setu-a67a7",
-//   storageBucket: "nagar-setu-a67a7.firebasestorage.app",
-//   messagingSenderId: "1014176543932",
-//   appId: "1:1014176543932:web:b3da579ea848838f6eccb9",
-//   measurementId: "G-YEF2DHSS0L",
-// };
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
